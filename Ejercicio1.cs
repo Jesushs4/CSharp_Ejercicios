8c41e2d531d7774bcf39a08497e31ca29c643b81
Jesús Herrera <115071141+Jesushs4@users.noreply.github.com>
2023-10-17 14:27:09 +0200

    Ejercicios 1-2

---
+++ Ejercicio1.cs
@@ -0,0 +1,17 @@
+﻿namespace HolaMundo
+{
+    internal class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+
+            Console.WriteLine("Introduzca un radio");
+            int radius = int.Parse(Console.ReadLine());
+
+            double area = Math.Pow(radius, 2)*PI;
+            Console.WriteLine("El area es de "+area);
+
+        }
+    }
+}
---
+++ Ejercicio2.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejercicio2
+{
+    internal class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cuál es tu edad?");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad < 18)
+            {
+                Console.WriteLine("No puedes conducir.");
+            }
+            else
+            {
+                Console.WriteLine("¿Tienes carnet? (S/N)");
+                string tieneCarnet = Console.ReadLine();
+
+                if (tieneCarnet.Equals("S"))
+                {
+                    Console.WriteLine("Puedes conducir.");
+                }
+                else
+                {
+                    Console.WriteLine("No puedes conducir sin carnet.");
+                }
+            }
+        }
+    }
+}
